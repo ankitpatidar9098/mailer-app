524161eb7402c2a038dc625f2db79222f3f95c12
Ankit Patidar <ankitpatidar01650@gmail.com>
2023-12-15 16:00:04 +0530

    send msg to multiple emaill config

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -20,4 +20,24 @@ class WelcomeController < ApplicationController
         send_email_with_retry(recipient)
       end
     end
- 
+  
+    private
+  
+    def send_email_with_retry(recipient, retry_attempts = 3)
+      attempts = 0
+  
+      begin
+        WelcomeMailer.send_welcome_mail(recipient).deliver_later
+      rescue SocketError => e
+        attempts += 1
+        if attempts < retry_attempts
+          sleep(1) # Wait for 1 second before retrying
+          retry
+        else
+          # Log or handle the error
+          Rails.logger.error("Failed to send email to #{recipient}: #{e.message}")
+        end
+      end
+    end
+  end
+  
